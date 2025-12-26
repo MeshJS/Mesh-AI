df0ad5e0ffd47e11616ee737c22ce607b02f7e8c
Santosh <santosh.mutyala.dev@gmail.com>
2025-12-26 16:53:41 +0100

    update team and ai docs

--- apps/docs/data/team.ts
+++ apps/docs/data/team.ts
@@ -1,15 +1,10 @@
 export const team = [
   {
-    name: "Abdelkrim",
-    image: "abdelkrim.png",
-    twitter: "https://x.com/abdelkrimdev",
-    github: "https://github.com/abdelkrimdev",
-  },
-  {
-    name: "Erick",
-    image: "erick.jpg",
-    twitter: "https://x.com/ErickRomeroEdda",
-    github: "https://github.com/ErickRomeroDev",
+    name: "Jingles",
+    image: "jingles.png",
+    twitter: "https://x.com/jinglescode",
+    github: "https://github.com/jinglescode",
+    website: "https://jingles.dev/",
   },
   {
     name: "Felix",
@@ -18,21 +13,43 @@ export const team = [
     github: "https://github.com/Felix-at-Swarm",
   },
   {
+    name: "Erick",
+    image: "erick.jpg",
+    twitter: "https://x.com/ErickRomeroEdda",
+    github: "https://github.com/ErickRomeroDev",
+  },
+  {
     name: "Hinson",
     image: "hinson.png",
     twitter: "https://x.com/HinsonSIDAN",
     github: "https://github.com/HinsonSIDAN",
   },
   {
-    name: "Jingles",
-    image: "jingles.png",
-    twitter: "https://x.com/jinglescode",
-    github: "https://github.com/jinglescode",
-    website: "https://jingles.dev/",
-  },
-  {
     name: "Tszwai",
     image: "tszwai.png",
     github: "https://github.com/twwu123",
+  },
+  {
+    name: "QS",
+    image: "qs.jpg",
+    github: "https://github.com/QSchlegel",
+  },
+  {
+    name: "Leif",
+    image: "leif.jpg",
+    linkedin: "https://www.linkedin.com/in/leifelliott/",
+    github: "https://github.com/JustLeif",
+  },
+  {
+    name: "Luis Lucena",
+    image: "luis-lucena.png",
+    twitter: "https://x.com/_luisald",
+    github: "https://github.com/luislucena16",
+  },
+  {
+    name: "Santosh",
+    image: "santosh.jpg",
+    github: "https://github.com/smutyala1at",
+    linkedin: "https://www.linkedin.com/in/smutyala2000/",
   },
 ];
